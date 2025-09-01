c604d08c1561e6f62f4f8e652689f776e6547d19
RonY <sunnyrawat225@gmail.com>
2025-09-02 02:23:24 +0530

    Added Search and Credit to themes

--- src/components/editor/theme-list.tsx
+++ src/components/editor/theme-list.tsx
@@ -3,18 +3,48 @@ import { cn } from "@/lib/utils";
 import { useTemplateStore } from "@/store/hooks";
 import { THEMES } from "@/lib/constants";
 import { THEME_INFO } from "@/lib/constants/constants";
+import React from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export default function ThemeList() {
+  const [query, setQuery] = React.useState("");
+  const [animationParent] = useAutoAnimate<HTMLDivElement>();
+  const filteredThemes = THEMES.filter((theme) =>
+    theme.name.toLowerCase().includes(query.toLowerCase())
+  );
   const templateStore = useTemplateStore();
   return (
-    <div className="flex flex-col gap-3 h-full pr-2">
-      {THEMES.map((theme) => {
+    <div ref={animationParent} className="flex flex-col gap-3 h-full pr-2">
+      <input
+        type="text"
+        placeholder="Search themes..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="input mx-1 mt-1 border border-base-300 p-2 rounded-3xl"
+      />
+      {filteredThemes.length === 0 && (
+        <>
+          <img
+            src="https://picscave.com/assetstore/medium/raiden_shogun_3_pYGrM-LaT7dAIZ.jpeg"
+            alt="No themes found"
+            className="w-full h-60 object-cover object-top rounded-3xl"
+          />
+          PS CHANGE LATER
+          <div className="text-sm text-base-content">No themes found</div>
+        </>
+      )}
+      <h2 className="text-lg font-semibold">Daisy UI Themes</h2>
+      <span className="divider my-0" />
+      {filteredThemes.map((theme) => {
         return (
           <div
             onClick={() => setActiveTheme(theme.name)}
             key={theme.id}
+                    style={{
+                    backgroundColor: theme.colors?.["--color-base-200"],
+                  }}
             className={cn(
-              "hover:bg-base-200 border border-base-300 rounded-2xl p-4 cursor-pointer transition-all duration-200 hover:shadow-md hover:scale-[1.02] group",
+              "hover:bg-base-200 border outline outline-white border-base-300 rounded-3xl p-4 cursor-pointer transition-all duration-200 hover:shadow-md hover:scale-[1.02] group",
               templateStore.active_theme.id === theme.name
                 ? "bg-base-200 shadow-lg"
                 : "bg-base-100",
@@ -38,11 +68,12 @@ export default function ThemeList() {
                 ];
 
                 return (
-                  <div className="grid grid-cols-2 gap-2">
+                  <div className="grid grid-cols-2 gap-2 p-2"
+          >
                     {colors.map((color, index) => (
                       <div
                         key={`color-${index}`}
-                        className="w-4 h-4 rounded-sm border border-white shadow-sm group-hover:scale-110 transition-transform duration-200"
+                        className="w-5 h-5 rounded-sm border border-white shadow-sm group-hover:scale-110 transition-transform duration-200"
                         style={{ backgroundColor: color }}
                         aria-hidden
                       />
@@ -52,7 +83,11 @@ export default function ThemeList() {
               })()}
 
               {/* Theme name */}
-              <span className="text-sm font-medium text-base-content capitalize tracking-wide">
+              <span
+                style={{
+                  color: theme.colors?.["--color-base-content"],
+                }}
+                className="text-sm font-medium capitalize tracking-wide">
                 {theme.name.toLocaleUpperCase()}
               </span>
             </div>
