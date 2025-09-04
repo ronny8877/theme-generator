426e0bc6b72bd7aff8293ec15100d41291a2ef53
nikhilswain <nikhilswain36@email.com>
2025-09-05 01:31:10 +0530

    modal template selection

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -2,25 +2,15 @@
 import TemplatePreview from "@/components/template-preview";
 import { ToolSelect } from "@/components/navs/tool-select";
 import DeviceSelect from "@/components/navs/device-select";
-import FloatingThemeSelector from "@/components/navs/app-theme";
-import FloatingSelect from "@/components/ui/floating-select";
-import { useState } from "react";
-import { TEMPLATES } from "@/lib/constants/constants";
+import TemplateSelector from "@/components/template-selector";
 import EditorToggle from "@/components/editor-toggle";
 // import ThemeInfo from "@/components/navs/theme-info";
 
 export default function TemplatesPage() {
-  const [selectedValue, setSelectedValue] = useState<string>("dashboard");
-
   return (
     <>
       {/* <ThemeInfo /> */}
-      <FloatingSelect
-        sections={TEMPLATES}
-        value={selectedValue}
-        onValueChange={setSelectedValue}
-        placeholder="Choose an option"
-      />
+      <TemplateSelector />
       <DeviceSelect />
       <div className="h-screen flex">
         {/* Template Preview */}
