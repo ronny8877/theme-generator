476d35b6109c4cd48754dc6ae1daa4c06eadff1c
RonY <sunnyrawat225@gmail.com>
2025-09-06 05:58:10 +0530

    minor patch

--- src/templates/website/landing.tsx
+++ src/templates/website/landing.tsx
@@ -358,7 +358,7 @@ export default function Landing() {
   };
 
   return (
-    <div className="min-h-screen bg-base-200/50 relative @container grid place-items-center w-full">
+    <div className="min-h-screen bg-base-200 relative @container grid place-items-center w-full">
       <div className="grid gap-6 grid-cols-1 @md:grid-cols-1 @3xl:grid-cols-2 items-start">
         <ClockCard
           city={ip?.city}
